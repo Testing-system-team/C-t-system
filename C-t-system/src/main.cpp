80803cb89068a2da0b4e405b69670739884f79ea
biguard1 <bogdasha05@gmail.com>
2023-03-05 19:29:44 +0200

    И ещё одно переименование запроса

--- C-t-system/src/main.cpp
+++ C-t-system/src/main.cpp
@@ -39,7 +39,7 @@ int main()
 		(*menuPtr)[L"Просмотр статистики"];
 	}
 
-	menuPtr->open(L"Ваш выбор");
+	menuPtr->open(L"Ваш выбор: ");
 }
 #else
 #include "Auth-System/Authorization.h"
@@ -76,7 +76,7 @@ int main()
 		(*menuPtr)[L"Просмотр статистики"];
 	}
 
-	menuPtr->open(L"Ваш выбор");
+	menuPtr->open(L"Ваш выбор: ");
 }
 #endif // 
 
